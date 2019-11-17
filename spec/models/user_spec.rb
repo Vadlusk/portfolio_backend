9d777313afccdd423be2014cffc206d488d71708
Adam Lusk <AdamLusk.Nexia@gmail.com>
2019-11-16 21:24:06 -0700

    Use let!(:user) instead of subject.

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -3,28 +3,26 @@ require 'rails_helper'
 describe User do
   let(:email)    { 'email@email.com' }
   let(:password) { 'password' }
-
-  subject { User.create(email: email, password: password) }
+  let!(:user)    { User.create(email: email, password: password) }
 
   describe 'field validations' do
     it 'is valid with all attributes' do
-      expect(subject).to be_valid
+      expect(user).to be_valid
     end
 
     it 'is invalid without an email' do
-      subject.update(email: nil)
+      user.update(email: nil)
 
-      expect(subject).to_not be_valid
+      expect(user).to_not be_valid
     end
 
     it 'is invalid without a password' do
-      subject.update(password: nil)
+      user.update(password: nil)
 
-      expect(subject).to_not be_valid
+      expect(user).to_not be_valid
     end
 
     it 'is invalid without a unique email' do
-      subject
       copy_cat = User.new(email: email, password: password)
 
       expect(copy_cat).to_not be_valid
