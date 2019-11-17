444f9624ed283008f4d947f8cf99927d1ed5c3e4
Adam Lusk <AdamLusk.Nexia@gmail.com>
2019-11-16 21:23:47 -0700

    Clean up spec and rails helpers.

--- spec/rails_helper.rb
+++ spec/rails_helper.rb
@@ -1,10 +1,14 @@
 ENV['RAILS_ENV'] ||= 'test'
-require File.expand_path('../config/environment', __dir__)
-abort("The Rails environment is running in production mode!") if Rails.env.production?
 
+require File.expand_path('../config/environment', __dir__)
 require 'rspec/rails'
 require 'spec_helper'
 require 'simplecov'
+require 'coveralls'
+
+abort("The Rails environment is running in production mode!") if Rails.env.production?
+
+Coveralls.wear!('rails')
 SimpleCov.start 'rails'
 
 Dir[Rails.root.join('spec', 'support', '**', '*.rb')].each { |f| require f }
