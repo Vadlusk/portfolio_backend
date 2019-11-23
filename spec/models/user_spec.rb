b9c047c8f99907b4f66e997346eebac792adbfdb
Adam Lusk <AdamLusk.Nexia@gmail.com>
2019-11-23 13:56:40 -0700

    Create it is invalid without shared example.

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -3,24 +3,15 @@ require 'rails_helper'
 describe User do
   let(:email)    { 'email@email.com' }
   let(:password) { 'password' }
-  let!(:user)    { User.create(email: email, password: password) }
+
+  subject!(:user) { User.create(email: email, password: password) }
 
   describe 'field validations' do
     it 'is valid with all attributes' do
       expect(user).to be_valid
     end
 
-    it 'is invalid without an email' do
-      user.update(email: nil)
-
-      expect(user).to_not be_valid
-    end
-
-    it 'is invalid without a password' do
-      user.update(password: nil)
-
-      expect(user).to_not be_valid
-    end
+    it_behaves_like 'it is invalid without', [:email, :password]
 
     it 'is invalid without a unique email' do
       copy_cat = User.new(email: email, password: password)
