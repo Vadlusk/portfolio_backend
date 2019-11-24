48f9cdc3902cea72a2184a9eb800943a8f57a824
Adam Lusk <AdamLusk.Nexia@gmail.com>
2019-11-23 18:53:04 -0700

    Change name of project to MyCoinCollectionBackend.

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -51,7 +51,7 @@ Rails.application.configure do
 
   # Use a real queuing backend for Active Job (and separate queues per environment).
   # config.active_job.queue_adapter     = :resque
-  # config.active_job.queue_name_prefix = "rails_jwt_auth_production"
+  # config.active_job.queue_name_prefix = "my_coin_collection_backend_production"
 
   config.action_mailer.perform_caching = false
 
