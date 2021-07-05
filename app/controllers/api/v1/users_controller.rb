79fee099e2e245e0aefe2b7309d54d1216923233
AdamLusk-Trane <adam@bouldercode.io>
2021-07-05 08:22:42 -0600

    refactor: set_user in users delete method

--- app/controllers/api/v1/users_controller.rb
+++ app/controllers/api/v1/users_controller.rb
@@ -2,6 +2,7 @@ class Api::V1::UsersController < ApplicationController
   before_action :authenticate_client_id, only: %i[authenticate create]
   before_action :authenticate_jwt, only: %i[destroy]
   before_action :check_ownership, only: %i[destroy]
+  before_action :set_user, only: %i[destroy]
 
   def authenticate
     @user = User.find_by_email!(user_params[:email])
@@ -18,12 +19,16 @@ class Api::V1::UsersController < ApplicationController
   end
 
   def destroy
-    User.find(params[:id]).destroy!
+    @user.destroy
   end
 
   private
 
     def user_params
       params.permit(:email, :password)
+    end
+
+    def set_user
+      @user ||= User.find(params[:id])
     end
 end
