253c88344ba14ff077b64c31a36f343950a6d94a
AdamLusk-Trane <adam@bouldercode.io>
2021-07-22 11:45:28 -0600

    refactor: clean up asset model

--- app/models/asset.rb
+++ app/models/asset.rb
@@ -1,7 +1,5 @@
 class Asset < ApplicationRecord
-  validates :currency, presence: true
-  validates :balance, presence: true
-  validates :remote_id, presence: true
+  validates_presence_of :remote_id, :current_balance, :currency 
 
   belongs_to :account
 end
--- spec/models/asset_spec.rb
+++ spec/models/asset_spec.rb
@@ -4,8 +4,8 @@ describe Asset do
   subject!(:asset) { Asset.create!(
     remote_id: 'remote_id',
     account_id: create(:account, user_id: create(:user)[:id])[:id],
-    balance: '39948.48',
-    currency: 'BTC'
+    current_balance: '39948.48',
+    currency: 'BTC-USD'
   ) }
 
   describe 'field validations' do
@@ -13,7 +13,7 @@ describe Asset do
       expect(asset).to be_valid
     end
 
-    it_behaves_like 'it is invalid without', [:remote_id, :account_id, :balance, :currency]
+    it_behaves_like 'it is invalid without', [:remote_id, :account_id, :current_balance, :currency]
   end
 
   describe 'relationships' do
